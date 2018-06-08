f4f98e6a41fc2681cd390f5cd889b64984af7d1e
Naykon <naykonbg@gmail.com>
2018-06-08 23:10:47 +0300

    Fixed broken alerts. Fixed shop items links. Items can now be shown by category. Added modals to show full details for individual items.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,10 +3,50 @@ $(document).ready(() => {
     loadRecentItems();
 });
 
+//Fix alerts so they can be hidden and not removed
+$(document).on("click", '#alertCloseBtn', function (e) {
+    e.preventDefault();
+    $(this).parent().hide();
+});
+
 $(document).on("click", '.categoryBtn', function (e) {
     e.preventDefault();
-    console.log("I was clicked");
-    $(".alert").show("fade");
+    let itemsURL = $(e.target).attr("href");
+    $.ajax({
+        method: "GET",
+        url: itemsURL,
+        success: function (itemsInACategory) {
+            //TODO: Add paging
+            $("#main-content-div").html("");
+            for (let oneItem of itemsInACategory["_embedded"]["items"]) {
+                $("#main-content-div").append(drawOneItem(oneItem));
+            }
+        },
+        error: function (error) {
+            $(".alert").show("fade");
+        }
+    });
+});
+
+$(document).on("click", '.itemBtn', function (e) {
+    e.preventDefault();
+    let itemURL = $(e.target).attr("href");
+
+    $.ajax({
+        method: "GET",
+        url: itemURL,
+        success: function (item) {
+            $("#modalItemName").html(item["name"]);
+            $("#modalItemPrice").html('€ ' + item["price"]);
+            $("#modalItemImageURL").attr("src", item["pictureURL"]);
+            $("#modalItemDescription").html(item["description"]);
+        },
+        error: function (error) {
+            $(".alert").show("fade");
+        }
+    });
+
+    $(".modal").modal("show");
 });
 
 function loadCategories() {
@@ -14,16 +54,13 @@ function loadCategories() {
         method: "GET",
         url: "/categories",
         success: function (categories) {
-            //console.log(categories);
+
             for (let category of categories["_embedded"]["categories"]) {
-                //TODO: Fix links
                 let name = category["name"];
-                let link = category["_links"]["self"]["href"];
-                let html = `<a href="#" class="list-group-item categoryBtn">${name}</a>`;
-                //html.data("category-id", link)
-                //html.data('category-id');
+                let relativeLink = formatURL(category["_links"]["items"]["href"]);
+                let html = `<a href="${relativeLink}" class="list-group-item categoryBtn">${name}</a>`;
+
                 $(".list-group").append(html);
-                //console.log(link);
             }
         },
         error: function (error) {
@@ -49,13 +86,21 @@ function loadRecentItems() {
 
 function drawOneItem(item) {
 
+    let link = '';
+
+    try {
+        link = formatURL(item ["_links"]["self"]["href"]);
+    } catch (e) {
+        link = `/items/${item["id"]}`;
+    }
+
     let html =
-        `            <div class="col-lg-4 col-md-6 mb-4">
+           `<div class="col-lg-4 col-md-6 mb-4">
               <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="${item["pictureURL"]}" alt=""></a>
+                <a><img class="card-img-top w-100 h-100" src="${item["pictureURL"]}" alt=""></a>
                 <div class="card-body">
                   <h4 class="card-title">
-                    <a href="#">${item["name"]}</a>
+                    <a href="${link}" class="itemBtn">${item["name"]}</a>
                   </h4>
                   <h5>${item["price"]}</h5>
                   <p class="card-text">${formatDescription(item["description"])}</p>
@@ -73,4 +118,9 @@ function formatDescription(description) {
     else {
         return description;
     }
+}
+
+function formatURL(url) {
+    //TODO: Find a way to handle this better. I can see no option to get relative URIs
+    return url.substring(21);
 }
